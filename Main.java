d67a61deae5f49ec7763b768be12b89efabb9cee
Vighnesh Nalla <vighneshnalla17@gmail.com>
2026-01-22 19:03:38 +0530

    readme.md

--- Main.java
+++ Main.java
@@ -1,44 +1,49 @@
-class Complex {
-    int real, imag;
+class Square{
+   int length;
 
-    Complex() {
-        real = 0;
-        imag = 0;
+   Square(int l){
+    length=l;
+   }
+
+   void area(){
+    int s_area = length*length;
+    System.out.println("Area of Square :"+s_area);
+   }
+}
+
+class Rectangle extends Square{
+    int breadth;
+
+    Rectangle(int l, int b){
+        super(l);
+        breadth=b;
     }
 
-    Complex(int r, int i) {
-        real = r;
-        imag = i;
+    void rect_area(){
+        int re_area=length*breadth;
+        System.out.println("Area of Rectangle:"+re_area);
+    }   
+}
+
+class Box extends Rectangle{
+    int height;
+
+    Box (int l,int b,int h){
+        super(l,b);
+        height=h;
     }
 
-    Complex(Complex c) {
-        real = c.real;
-        imag = c.imag;
-    }
-
-    Complex add(Complex c) {
-        Complex temp = new Complex();
-        temp.real = this.real + c.real;
-        temp.imag = this.imag + c.imag;
-        return temp;
-    }
-
-    void display() {
-        System.out.println(real + " + " + imag + "i");
+    void volume(){
+        int bo_area=length*breadth*height;
+        System.out.println("Area of Box:"+bo_area);
     }
 }
 
-public class Main {
+class Main{
     public static void main(String[] args) {
-        Complex c1 = new Complex(3, 4);
-        Complex c2 = new Complex(5, 6);
-
-        Complex c3 = c1.add(c2);
-        Complex c4 = new Complex(c3);
-
-        c1.display();
-        c2.display();
-        c3.display();
-        c4.display();
+        Box b = new Box(7,6,8);
+        b.area();
+        b.rect_area();
+        b.volume();
     }
 }
